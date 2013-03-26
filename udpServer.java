972ec6c30745df6360bfd70fd2944969cfe58f02
kaisejoe <kaisejoe@mail.gvsu.edu>
2013-03-26 16:00:21 -0300

    Update udpServer.java

--- udpServer.java
+++ udpServer.java
@@ -2,15 +2,15 @@ import java.io.*;
 import java.net.*;
 
 class udpServer{
-  public static void main(String args[]) throws Exception{
-		DatagramSocket serverSocket = new DatagramSocket(9876);
+	public static void main(String args[]) throws Exception{
+		DatagramSocket serverSocket = new DatagramSocket(9876, "148.61.112.72");
 		while(true){
 			byte[] recvData = new byte[1024];
 			byte[] sendData = new byte[1024];
 			DatagramPacket recvPacket = new DatagramPacket(recvData, recvData.length);
 			serverSocket.receive(recvPacket);
-			String message = new String(recvPacket.getData()).trim();
-			String newMessage = "Server received: " + message + ". Thank you, come again!\n";
+			String fileName = new String(recvPacket.getData()).trim();
+			String newMessage = fileName + " - Sending shortly! \n";
 			InetAddress IPAddress = recvPacket.getAddress();
 			int port = recvPacket.getPort();
 			sendData = newMessage.getBytes();
