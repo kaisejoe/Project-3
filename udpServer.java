0f3f115827fbb963b75709e348336a9109e73067
macdonad <macdonad@mail.gvsu.edu>
2013-04-09 16:07:43 -0300

    make check sum

--- udpServer.java
+++ udpServer.java
@@ -7,15 +7,35 @@ class udpServer{
 		while(true){
 			byte[] recvData = new byte[1024];
 			byte[] sendData = new byte[1024];
+			
 			DatagramPacket recvPacket = new DatagramPacket(recvData, recvData.length);
 			serverSocket.receive(recvPacket);
+			
 			String fileName = new String(recvPacket.getData()).trim();
 			String newMessage = fileName + " - Sending shortly! \n";
+			
 			InetAddress IPAddress = recvPacket.getAddress();
+			
 			int port = recvPacket.getPort();
+			
 			sendData = newMessage.getBytes();
+			makeCheckSum(sendData);
+			
+			
+			
 			DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
 			serverSocket.send(sendPacket);
 		}
+	}
+	
+	public int makeCheckSum(byte[] sendData){
+		byte[] data = sendData;
+		int checkSum = 0;
+		
+		for(int i = 0; i < data.length(); i++){
+			checkSum += (int)data[i];			
+		}
+		
+		return checkSum;
 	}
 }
