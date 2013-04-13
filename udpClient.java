513212f34abaf800156e5a2ac3b28dc763ec4d07
Brett Barrett <barrebre@gmail.com>
2013-04-13 13:54:53 -0300

    updated a lot
    1) only add to completed arraylist if checksum is correct
    2) made an arraylist for missing data. only that data will need to be requested again. it doesn't work completely right now
    3) wrote the checkData method

--- udpClient.java
+++ udpClient.java
@@ -20,6 +20,10 @@ class udpClient{
 		System.out.print("Filename: ");
 		fileName = inFromUser.readLine();
 		byte[] sendData = new byte[1024];
+		//what if the filename takes more than 1024 bytes? i highly doubt this but it
+		////seems like we should have to make a loop that will just copy the bytes into
+		////the array into place. otherwise, the server may be waiting for bytes that it
+		////won't receive if the fileName.getBytes is less than 1024
 		sendData = fileName.getBytes();
 	
 		DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
@@ -34,20 +38,51 @@ class udpClient{
 		
 		//this should keep receiving data, then put it in the arraylist based
 		//on its sequence number - Brett
+		byte[] recvData = new byte[1024];
+		DatagramPacket recvPacket = new DatagramPacket(recvData, recvData.length);
 		for(int i=0; i<packetCount; i++){
-			byte[] recvData = new byte[1024];
-			DatagramPacket recvPacket = new DatagramPacket(recvData, recvData.length);
 			clientSocket.receive(recvPacket);
 			recvData = recvPacket.getData();
 			
 			byte[] actualData = analyzeData(recvData);
 			
-			packetData.add(analyzeSeqNum(recvData), actualData);
+			if(checkCheckSum)
+				packetData.add(analyzeSeqNum(recvData), actualData);
 		}
 		
-		//I don't know what this is supposed to do - Brett
+		//after we've tried every packet, we only need to retry the packets that didn't 
+		////yield the correct checksum - brett
+		ArrayList<Integer> missingPackets = ArrayList<Integer>();
+		missingPackets = checkData(packetData);
+		
+		//on the fly, i wasn't able to think of a way to optimize this loop with the
+		////previous one - brett
+		//what method do we write for the server to tell it to only send the selected
+		////packets?
+		while(missingPackets.size()>0){
+			clientSocket.receive(recvPacket);
+			recvData = recvPacket.getData();
+			
+			
+			byte[] actualData = analyzeData(recvData);
+			
+			if(checkCheckSum)
+				packetData.add(analyzeSeqNum(recvData), actualData);
+		}
+		
+		//once the missing packets are 0, we can send the ack for all correct data. really,
+		////i don't know why we'd need to send an ack for the whole data. it seems like if
+		////we were going about our missing data in a different fashion, it'd be more
+		////appropriate to send acks for every piece of correct data. the way we are now,
+		////i don't think the ack is necessary. - brett
+		sendAck();
+		
+		
+		//I don't know what this is supposed to do. the server isn't really saying anything,
+		////it's receiving a file. - Brett
 		String newMessage = new String(recvData);
 		System.out.println("Server says: " + newMessage);
+		
 		clientSocket.close();
 	}
 	
@@ -117,5 +152,20 @@ class udpClient{
 			fos.write(b.toArray());
 		}
 		}catch(IOException e){System.out.println("Error writing to file "+filename);}
+	}
+	
+	private ArrayList<Integer> checkData(ArrayList<Byte[]> packetData){
+		ArrayList<Integer> missing = new ArrayList<Integer>();
+		int size = packetData.size();
+		for(int i=0; i<size; i++){
+			if(packetData.get(i)==null)
+				missing.add(i);
+		}
+		return missing;
+	}
+	
+	//blank because i don't know what we're going to send - brett
+	private sendAck(){
+		
 	}
 }
