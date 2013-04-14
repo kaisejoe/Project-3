68562dba4eed9e9e324ee71cecf5ca8d37f7eb10
Doug MacDonald <macdonad@mail.gvsu.edu>
2013-04-14 14:32:18 -0300

    Update: Added in Extended Header Content in the Fragment Data...
    Update: Added in Extended Header Content in the Fragment Data... Also updated the sending of data to send the fragmented data.

--- udpServer.java
+++ udpServer.java
@@ -21,8 +21,15 @@ class udpServer{
 			sendData = newMessage.getBytes();
 			makeCheckSum(sendData);
 			
-			DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
-			serverSocket.send(sendPacket);
+			
+			byte[][] fragSendData = fragmentData(sendData);
+			int totalNumberOfPackets = fragSendData.length;
+			
+			for(int i = 0; i < totalNumberOfPackets; i++){
+				DatagramPacket sendPacket = new DatagramPacket(fragSendData[i], fragSendData[i].length, IPAddress, port);
+				serverSocket.send(sendPacket);
+			}		
+			
 		}
 	}
 	
@@ -44,12 +51,19 @@ class udpServer{
 	
 	//Method that fragments data into appropriate sizes in an array
 	//928 bits of data = 116 bytes	
+	// Also adds in the extra data for the extended header
 	private static byte[][] fragmentData(byte[] sendData){	    
 		int length = sendData.length;
     		int numOfFrags = (length / 116) + 1;
-		byte[][] fragData = new byte[numOfFrags][116];
+		byte[][] fragData = new byte[numOfFrags][120];
 		for(int counter = 0; counter < length; counter++){
-			for(int counter2 = 0; counter2 < 116; counter2++){
+			byte[] seqNum = ByteBuffer.allocate(2).putInt(counter).array();
+			byte[] totalNum = ByteBuffer.allocate(2).putInt(numOfFrags).array();
+			fragData[counter][0] = seqNum[0];
+			fragData[counter][1] = seqNum[1];
+			fragData[counter][2] = totalNum[0];
+			fragData[counter][3] = totalNum[1];
+			for(int counter2 = 4; counter2 < 120; counter2++){
 				fragData[counter][counter2] = sendData[counter];
 			}
 		}
