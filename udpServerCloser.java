00e2975f6e01aada2346fa87c9fd5d143439ed70
kaisejoe <kaisejoe@mail.gvsu.edu>
2013-04-15 23:05:57 -0300

    Update udpServerCloser.java
    Checksum is not being sent whatsoever. Lots of issues still. Text files transfer relatively OK and very small images (although not all of the pixels are in the right place).

--- udpServerCloser.java
+++ udpServerCloser.java
@@ -1,114 +1,200 @@
 import java.io.*;
 import java.net.*;
-import java.nio.*;
-import java.nio.file.*;
 import java.util.*;
 
-class udpServer{
-  public static void main(String args[]) throws Exception{
-		DatagramSocket serverSocket = new DatagramSocket(9876);
-		while(true){
-			byte[] recvData = new byte[1016];
-			byte[] sendData;// = new byte[1024];
+class udpClient{
 
-			DatagramPacket recvPacket = new DatagramPacket(recvData, recvData.length);
-			serverSocket.receive(recvPacket);
+	static String fileName;
 
-			String fileName = new String(recvPacket.getData()).trim();
-			System.out.println(fileName);
-			
-			try{
-				File file = new File(fileName);	
-				sendData = Files.readAllBytes(file.toPath());
+	public static void main(String args[]) throws Exception{
+		int port;
+		DatagramSocket clientSocket = new DatagramSocket();
+		clientSocket.setSoTimeout(2000);
+		BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+		System.out.print("IP Address: ");
+		//String address = new String(inFromUser.readLine());
+		String address = new String("148.61.112.70");
+		InetAddress IPAddress = InetAddress.getByName(address);
 
-			InetAddress IPAddress = recvPacket.getAddress();
+		System.out.print("Port #: ");
+		//port = Integer.parseInt(inFromUser.readLine());
+		port = 9876;
 
-			int port = recvPacket.getPort();
+		System.out.print("Filename: ");
+		fileName = inFromUser.readLine();
+		byte[] sendData = new byte[1016];
+		//what if the filename takes more than 1024 bytes? i highly doubt this but it
+		////seems like we should have to make a loop that will just copy the bytes into
+		////the array into place. otherwise, the server may be waiting for bytes that it
+		////won't receive if the fileName.getBytes is less than 1024
+		sendData = fileName.getBytes();
 
-			
-			//int checkSum = makeCheckSum(sendData);
+		DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
+		clientSocket.send(sendPacket);
 
-			byte[][] fragSendData = fragmentData(sendData);
-			int totalNumberOfPackets = fragSendData.length;
-			System.out.println("Num packets: " + totalNumberOfPackets);
-			for(int i = 0; i < totalNumberOfPackets; i++){
-				DatagramPacket sendPacket = new DatagramPacket(fragSendData[i], fragSendData[i].length, IPAddress, port);
-				serverSocket.send(sendPacket);
-			}
+		byte[] recvData = new byte[1016];
+		DatagramPacket recvPacket = new DatagramPacket(recvData, recvData.length);
+		clientSocket.receive(recvPacket);
+		recvData = recvPacket.getData();
 
-			//receive more data to check for missing or incorrect packet transmission
-			int numLeft = 1;
-			while(numLeft > 0){
-				System.out.println("Entered resend cycle...");
-				serverSocket.receive(recvPacket);
-				String missing = new String(recvPacket.getData()).trim();
-				
-				System.out.println(missing);
-
-				List<String> missingList = new ArrayList<String>(Arrays.asList(missing.split(",")));
-				numLeft = missingList.size();
-
-				if(numLeft>0){
-					for(int i = 0; i < numLeft; i++){
-						try{
-							int packetNum = Integer.parseInt(missingList.get(i));
-							DatagramPacket sendPacket = new DatagramPacket(fragSendData[packetNum], fragSendData[packetNum].length, IPAddress, port);
-							serverSocket.send(sendPacket);
-						}catch(NumberFormatException b){
-							missingList.remove(i);
-							numLeft--;
-						}
-					}
-				}
-			}
-			System.out.println("Left resend cycle...");
-			}catch(FileNotFoundException e){
-				System.out.println("Invalid file.");
-			}
+		int packetCount =  (int)(analyzePacketTotal(recvData));
+		ArrayList<byte[]> packetData = new ArrayList<byte[]>();
+		for(int m = 0; m < packetCount; m++){
+			packetData.add(null);
 		}
+		System.out.println("Num packets: " + packetCount + " length: " + packetData.size());
+
+		//this should keep receiving data, then put it in the arraylist based
+		//on its sequence number - Brett
+		//DatagramPacket
+		recvPacket = new DatagramPacket(recvData, recvData.length);
+		try{
+			for(int i=0; i<packetCount; i++){	
+				recvData = new byte[1016];
+				clientSocket.receive(recvPacket);
+				recvData = recvPacket.getData();
+			
+			
+				byte[] actualData = analyzeData(recvData);
+				String temp = new String(actualData, "UTF-8");
+				System.out.println("Received data: " + temp);
+				//if(checkCheckSum(0, recvData))
+				packetData.set((int)analyzeSeqNum(recvData), actualData);
+			}
+		}catch(SocketTimeoutException e){
+			System.out.println("Timeout.");
+		}
+		System.out.println("Received all data!");
+
+		//after we've tried every packet, we only need to retry the packets that didn't 
+		////yield the correct checksum - brett
+		ArrayList<Integer> missingPackets = new ArrayList<Integer>();
+		missingPackets = checkData(packetData);
+		int missingCount = missingPackets.size();
+		String missingString;
+		System.out.println("Missing count: " + missingCount);
+		do{
+			missingString = "";
+			for(int i=0; i<missingCount; i++){
+				missingString+=missingPackets.get(i)+",";
+			}
+			System.out.println("Missing: " + missingString);
+			sendData = missingString.getBytes();
+
+			sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
+			clientSocket.send(sendPacket);
+
+			for(int i=0; i<missingCount; i++){	
+				recvData = new byte[1016];
+				clientSocket.receive(recvPacket);
+				recvData = recvPacket.getData();
+				System.out.println(recvData.length);
+
+				byte[] actualData = analyzeData(recvData);
+
+				//if(checkCheckSum(0, recvData))
+					packetData.set(analyzeSeqNum(recvData), actualData);
+			}
+
+			missingPackets = checkData(packetData);
+			missingCount = missingPackets.size();
+			System.out.println(missingCount);
+		} while(missingCount>0);
+
+
+		//I don't know what this is supposed to do. the server isn't really saying anything,
+		////it's receiving a file. - Brett
+		//String newMessage = new String(recvData);
+		//System.out.println("Server says: " + newMessage);
+		
+		writeToFile(packetData);
+		clientSocket.close();
+		
+		
 	}
 
-	//changed the copy of the array to make sure there was no pointer issue. - brett
-	//changed the checksum value. even if the client received incorrect data, it would've all
-	////been counted and would've appeared to be correct.
-	private static int makeCheckSum(byte[] sendData){
-		byte[] data = (byte[])sendData.clone();
-
-		int checkSum = 0;
-
-		for(int i = 0; i < data.length; i++){
-			if(data[i]==1)
-				checkSum += (int)data[i];			
+	private static boolean checkCheckSum(int checkSum, byte[] recvData){
+		int recvCheckSum = checkSum;
+		int ourCheckSum = 0;
+		for(int i =0; i < recvData.length; i++){
+			if(recvData[i]==1)
+			ourCheckSum += (int)recvData[i];
 		}
 
-		return checkSum;
+		if(ourCheckSum == recvCheckSum){
+			return true;
+		}else{
+			return false;
+		}	
 	}
 
-	//Method that fragments data into appropriate sizes in an array
-	//928 bits of data = 116 bytes	
-	// Also adds in the extra data for the extended header
-	private static byte[][] fragmentData(byte[] sendData){	    
-		int pos = 0;
-		int length = sendData.length;
-		System.out.println(length);
-    	short numOfFrags = (short)((length / 1012) + 1);
-		byte[][] fragData = new byte[numOfFrags][1016];
-		for(short counter = 0; counter < numOfFrags; counter++){
-			byte[] seqNum = ByteBuffer.allocate(2).putShort(counter).array();
-			byte[] totalNum = ByteBuffer.allocate(2).putShort(numOfFrags).array();
-			fragData[counter][0] = seqNum[0];
-			fragData[counter][1] = seqNum[1];
-			fragData[counter][2] = totalNum[0];
-			fragData[counter][3] = totalNum[1];
-			int counter2 = 4;
-			while(pos < length && counter2 < 1016){
-			//for(int counter2 = 4; counter2 < 1016; counter2++){
-				fragData[counter][counter2] = sendData[pos];
-				counter2++;
-				pos++;
-			}
-			System.out.println("Num frags: " + numOfFrags);
+	//Get data from packet
+	private static byte[] analyzeData(byte[] recvData){
+		byte[] data = new byte[1016];
+		for(int i = 4; i < recvData.length; i++){
+			data[i-4] = recvData[i];	
 		}
-		return fragData;		
+		return data;
+	}
+
+	//Get sequence number header from packet
+	private static short analyzeSeqNum(byte[] recvData){
+		byte[] data = new byte[2];
+		for(int i = 0; i < 2; i++){
+			data[i] = recvData[i];	
+		}
+		//found this online for convering from byte[] to int
+		//http://stackoverflow.com/questions/5399798/byte-array-and-int-conversion-in-java
+		// - Brett
+		//int index = 0;
+    		short value = (short) ((data[0] << 8) | (data[1]& 0xFF));
+    		/*int value = data[index++] << Byte.SIZE * 3;
+    		value ^= (data[index++] & 0xFF) << Byte.SIZE * 2;
+    		value ^= (data[index++] & 0xFF) << Byte.SIZE * 1;
+    		value ^= (data[index++] & 0xFF);*/
+			System.out.println("Seq num: " + value);
+    		return value;
+	}
+
+	//Get packet total header from packet
+	private static short analyzePacketTotal(byte[] recvData){
+		byte[] data = new byte[2];
+		System.out.println(recvData.length);
+		for(int i = 2; i < 4; i++){
+			data[i-2] = recvData[i];	
+		}
+		//found this online for convering from byte[] to int
+		//http://stackoverflow.com/questions/5399798/byte-array-and-int-conversion-in-java
+		// - Brett
+		//int index = 0;
+			short value = (short) (((data[0]) << 8) | (data[1]& 0xFF));
+    		/*int value = data[index++] << Byte.SIZE * 3;
+    		value ^= (data[index++] & 0xFF) << Byte.SIZE * 2;
+    		value ^= (data[index++] & 0xFF) << Byte.SIZE * 1;
+    		value ^= (data[index++] & 0xFF);*/
+			System.out.println("Total packets: " + value);
+    		return value;
+	}
+
+	//Write the values we've received to filename
+	private static void writeToFile(ArrayList<byte[]> packetData){  //throws IOException? or just try/catch?
+		try{
+			FileOutputStream fos = new FileOutputStream(new File("temp" + fileName));
+			for(byte[] b : packetData){
+				fos.write(b);
+			}
+			fos.close();
+		}catch(IOException e){System.out.println("Error writing to file " + fileName);}
+	}
+
+	private static ArrayList<Integer> checkData(ArrayList<byte[]> packetData){
+		ArrayList<Integer> missing = new ArrayList<Integer>();
+		int size = packetData.size();
+		System.out.println(packetData.size());
+		for(int i=0; i<size; i++){
+			if(packetData.get(i)==null)
+				missing.add(i);
+		}
+		return missing;
 	}
 }
